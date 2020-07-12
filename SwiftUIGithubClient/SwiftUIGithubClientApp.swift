6551c8a966bfca2c74be9ea82ac31267cdca16ad
takaoh717 <hrgc.tko0717@gmail.com>
2020-07-12 14:31:55 +0900

    init

---
+++ SwiftUIGithubClient/SwiftUIGithubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGithubClientApp.swift
+//  SwiftUIGithubClient
+//
+//  Created by Takao Horiguchi on 2020/07/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGithubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
